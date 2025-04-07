bf1fcff3d0ff63b7c96151f80bfc13c818b9461b
ISrainstop <2212177156@qq.com>
2025-04-07 21:41:17 +0800

    初始化项目

---
+++ src/test/java/com/israinstop/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israinstop.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
